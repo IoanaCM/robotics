bb16253637678920be372a9e6614c70572cd9dc5
Chenery, Alfie <alfie.chenery20@imperial.ac.uk>
2023-01-31 16:52:53 +0000

    refactoring code

---
+++ constants.py
@@ -0,0 +1,6 @@
+from math import pi as PI
+#Robot measurements and calibrations
+
+wheel_radius = 2.8                          #radius of robot wheels
+robot_width = 24                            #width between wheels of robot
+wheel_circ = 2 * PI * wheel_radius          #circumference of robot wheels
--- square.py
+++ square.py
@@ -5,45 +5,59 @@
 #
 # Results:
 
-from __future__ import print_function # use python 3 syntax but make it compatible with python 2
-from __future__ import division       #                           ''
+#uncomment these if they were actually needed, else im deleting them
+#from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+#from __future__ import division
 
-import time     # import the time library for the sleep function
-import brickpi3 # import the BrickPi3 drivers
+import time
+from math import pi as PI
+import brickpi3
+from constants import *
 
-BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
-try:
+def init(BP):
     try:
         BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A)) # reset encoder A
         BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder D
+
+        BP.set_motor_limits(BP.PORT_A, 50) # Set power limits on motors
+        BP.set_motor_limits(BP.PORT_D, 50)
+
     except IOError as error:
         print(error)
-    
-    # BP.set_motor_power(BP.PORT_D, BP.MOTOR_FLOAT)                          # float motor D
-    BP.set_motor_limits(BP.PORT_A, 50)                                     # optionally set a power limit
-    BP.set_motor_limits(BP.PORT_D, 50)                                     # optionally set a power limit
-    wheel_radius = 2.8
-    robot_width = 24
-    wheel_circ = 2 * 3.14 * wheel_radius
-    dps = 360
-    dps2 = 355
-    square_side = 40
-    error = 0.1
-    t1 = square_side / (wheel_circ * dps / 360) + error # time to walk a side
-    rotate_distance = 2 * 3.14 * robot_width / 8
-    t2 = rotate_distance / (wheel_circ * dps / 360) - error/2
-    while True:
-        BP.set_motor_dps(BP.PORT_A, dps)             # set the target speed for motor A in Degrees Per Second
-        BP.set_motor_dps(BP.PORT_D, dps)
-        
-        time.sleep(t1)
-        
-        BP.set_motor_dps(BP.PORT_D, dps)
-        BP.set_motor_dps(BP.PORT_A, -dps)             # set the target speed for motor A in Degrees Per Second
-        
-        time.sleep(t2)
-        
-except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
 
+
+def main():
+    BP = brickpi3.BrickPi3()
+
+    try:
+        
+        init(BP)
+        
+        error = 0.1
+        square_side = 40 #cm
+        dps = 360 #target degrees per second speed
+
+        t1 = square_side / (wheel_circ * dps / 360) + error # time to walk a side
+
+        rotate_distance = PI * robot_width / 4
+        t2 = rotate_distance / (wheel_circ * dps / 360) - error/2 #time to spin 90 degrees
+
+        while True:
+            BP.set_motor_dps(BP.PORT_A, dps) #set the target speed for motor A in Degrees Per Second
+            BP.set_motor_dps(BP.PORT_D, dps)
+            
+            time.sleep(t1)
+            
+            BP.set_motor_dps(BP.PORT_D, dps)
+            BP.set_motor_dps(BP.PORT_A, -dps)
+            
+            time.sleep(t2)
+            
+    except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+        BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
+        return
+
+
+if __name__ == "__main__":
+    main()
