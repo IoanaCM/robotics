1d240f2b61673ade3a7e8dee6592d85638051f9e
Chenery, Alfie <alfie.chenery20@imperial.ac.uk>
2023-01-31 17:42:02 +0000

    Refactored control code into a robot class

---
+++ robot.py
@@ -0,0 +1,113 @@
+from math import pi as PI
+import time
+import brickpi3
+
+# Class specialised to our robot design
+
+class robot:
+
+    #=========== Constructor function ===========
+    def __init__(self):
+        self.BP = brickpi3.BrickPi3()
+
+        # design constants
+        self.L = self.BP.PORT_A     #Port used for left wheel
+        self.R = self.BP.PORT_D     #Port used for right wheel
+
+        # calibration constants
+        self.wheel_radius = 2.8     #radius of robot wheels (cm)
+        self.robot_width = 24       #width between wheels of robot (cm)
+        self.dps = 360              #desired wheel speed (degrees per second)     
+
+        self.error = 0.1            #manually calibrated error
+
+
+        #constants calculated from configurable constants (should not be changed)
+        self.wheel_circ = 2 * PI * self.wheel_radius            #circumference of robot wheels
+        self.wheel_speed = self.wheel_circ * self.dps / 360     #speed wheels should turn
+
+
+    #========== Private methods - Do not call directly ===========
+    def private_spin(self, direction, radians):
+        """
+        PRIVATE METHOD - DO NOT CALL THIS METHOD\n
+        Use spinL and spinR instead
+        """
+
+        #direction = 1 for spin left, -1 for spin right
+        t = radians / self.wheel_speed - self.error/2 #time to spin 90 degrees
+        self.BP.set_motor_dps(self.L, -direction * self.dps)
+        self.BP.set_motor_dps(self.R, direction * self.dps)
+
+        time.sleep(t)
+
+        self.BP.set_motor_dps(self.L, 0)
+        self.BP.set_motor_dps(self.R, 0)
+
+        self.stop()
+        return
+
+
+    #=========== Public methods ===========
+
+    def setup(self):
+        """
+        Initialises the motors. Should be called before using other functions
+        """
+
+        try:
+            #reset servo encoders
+            self.BP.offset_motor_encoder(self.L, self.BP.get_motor_encoder(self.L))
+            self.BP.offset_motor_encoder(self.R, self.BP.get_motor_encoder(self.R))
+
+            # Set power limits on motors
+            self.BP.set_motor_limits(self.L, 50) 
+            self.BP.set_motor_limits(self.R, 50)
+
+        except IOError as error:
+            print(error)
+
+        return
+
+    def shutdown(self):
+        """
+        Should be called at end of code\n
+        Unconfigures the sensors, disables the motors, and restores the LED to the control of the BrickPi3 firmware
+        """
+        self.BP.reset_all() 
+
+    def stop(self):
+        """
+        Stops both motors turning
+        """
+        self.BP.set_motor_dps(self.L, 0)
+        self.BP.set_motor_dps(self.R, 0)
+        return
+
+    def forward(self, distance):
+        """
+        Drives straight forward 'distance' cm
+        """
+        t = distance / self.wheel_speed + self.error # time to walk a side
+        self.BP.set_motor_dps(self.L, self.dps)
+        self.BP.set_motor_dps(self.R, self.dps)
+            
+        time.sleep(t)
+
+        self.stop()
+        return
+
+    def spinL(self, radians):
+        """
+        Spins 'radians' radians to the left in place
+        """
+        self.private_spin(1, radians)
+        return
+
+    def spinR(self, radians):
+        """
+        Spins 'radians' radians to the right in place
+        """
+        self.private_spin(-1, radians)
+        return
+
--- square.py
+++ square.py
@@ -9,53 +9,20 @@
 #from __future__ import print_function # use python 3 syntax but make it compatible with python 2
 #from __future__ import division
 
-import time
 from math import pi as PI
-import brickpi3
-from constants import *
-
-
-def init(BP):
-    try:
-        BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A)) # reset encoder A
-        BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder D
-
-        BP.set_motor_limits(BP.PORT_A, 50) # Set power limits on motors
-        BP.set_motor_limits(BP.PORT_D, 50)
-
-    except IOError as error:
-        print(error)
-
+import robot
 
 def main():
-    BP = brickpi3.BrickPi3()
+    r = robot.robot()
+    r.setup()
 
-    try:
-        
-        init(BP)
-        
-        error = 0.1
-        square_side = 40 #cm
-        dps = 360 #target degrees per second speed
-
-        t1 = square_side / (wheel_circ * dps / 360) + error # time to walk a side
-
-        rotate_distance = PI * robot_width / 4
-        t2 = rotate_distance / (wheel_circ * dps / 360) - error/2 #time to spin 90 degrees
-
+    try:    
         while True:
-            BP.set_motor_dps(BP.PORT_A, dps) #set the target speed for motor A in Degrees Per Second
-            BP.set_motor_dps(BP.PORT_D, dps)
-            
-            time.sleep(t1)
-            
-            BP.set_motor_dps(BP.PORT_D, dps)
-            BP.set_motor_dps(BP.PORT_A, -dps)
-            
-            time.sleep(t2)
+            robot.forward(40)
+            robot.spinL(PI/2)
             
     except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-        BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
+        robot.shutdown()       
         return
 
 
