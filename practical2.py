65ce2c1632e40d43779326c13da703b087075311
Ioana Mihailescu <ioana.mihailescu20@imperial.ac.uk>
2023-02-07 15:56:57 +0000

    feat: do stuff

--- practical2.py
+++ practical2.py
@@ -8,7 +8,8 @@
 from __future__ import print_function # use python 3 syntax but make it compatible with python 2
 from __future__ import division       #                           ''
 
-import math
+from math import pi as PI
+import robot
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 
@@ -26,7 +27,7 @@ try:
     BP.set_motor_limits(BP.PORT_D, 70)                                     # optionally set a power limit
     wheel_radius = 2.8
     robot_width = 22.5
-    wheel_circ = 2 * math.pi * wheel_radius
+    wheel_circ = 2 * PI * wheel_radius
     dps = 360
     dps2 = 360
     square_side = 40
@@ -34,52 +35,36 @@ try:
     error = 0.0725
     error2 = -0.0565
     t1 = stop_distance / (wheel_circ * dps / 360) + error # time to walk 10cm
-    rotate_distance = 2 * math.pi * robot_width / 8
+    rotate_distance = 2 * PI * robot_width / 8
     t2 = rotate_distance / (wheel_circ * dps / 360) + error2 # time to rotate
-    i = 0
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-    print("Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-    print("Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-    while i<4:
-        i=i+1
-        print("Side: ", i, "  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-        print("Side: ", i, "  Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-        BP.set_motor_dps(BP.PORT_A, dps)             # set the target speed for motor A in Degrees Per Second
-        BP.set_motor_dps(BP.PORT_D, dps2)
-        time.sleep(t1)
-        BP.set_motor_dps(BP.PORT_D, 0)
-        BP.set_motor_dps(BP.PORT_A, 0)
-        print("Side: ", i, "  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-        print("Side: ", i, "  Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-        BP.set_motor_dps(BP.PORT_A, dps)             # set the target speed for motor A in Degrees Per Second
-        BP.set_motor_dps(BP.PORT_D, dps2)
-        time.sleep(t1)
-        BP.set_motor_dps(BP.PORT_D, 0)
-        BP.set_motor_dps(BP.PORT_A, 0)
-        print("Side: ", i, "  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-        print("Side: ", i, "  Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-        BP.set_motor_dps(BP.PORT_A, dps)             # set the target speed for motor A in Degrees Per Second
-        BP.set_motor_dps(BP.PORT_D, dps2)
-        time.sleep(t1)
-        BP.set_motor_dps(BP.PORT_D, 0)
-        BP.set_motor_dps(BP.PORT_A, 0)
-        print("Side: ", i, "  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-        print("Side: ", i, "  Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-        BP.set_motor_dps(BP.PORT_A, dps)             # set the target speed for motor A in Degrees Per Second
-        BP.set_motor_dps(BP.PORT_D, dps2)
-        time.sleep(t1)
-        BP.set_motor_dps(BP.PORT_D, 0)
-        BP.set_motor_dps(BP.PORT_A, 0)
-        print("Side: ", i, "  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-        print("Side: ", i, "  Motor D Status: ", BP.get_motor_status(BP.PORT_D))
-        BP.set_motor_dps(BP.PORT_A, dps2)
-        BP.set_motor_dps(BP.PORT_D, -dps)             # set the target speed for motor A in Degrees Per Second
-        time.sleep(t2)
-        BP.set_motor_dps(BP.PORT_D, 0)
-        BP.set_motor_dps(BP.PORT_A, 0)
-    print("Motor A Status: ", BP.get_motor_status(BP.PORT_A))
-    print("Motor D Status: ", BP.get_motor_status(BP.PORT_D))
+
+    r = robot.robot()
+    r.setup()
+
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.spinL(PI/2)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.spinL(PI/2)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.spinL(PI/2)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.forward(10)
+    r.spinL(PI/2)
+    
+
+    
+    
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
 
